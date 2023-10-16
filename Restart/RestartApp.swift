d2553b0baf682f5c0c98cf26fab1537fb7f37958
Erik Mikac <5546184+HankSchrader@users.noreply.github.com>
2023-10-16 19:59:29 -0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Erik Mikac on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
